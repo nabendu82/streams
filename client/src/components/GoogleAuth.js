ee074f2bbb604ff6fcaaaa102c94306ad322346c
Nabendu Biswas <nabendubiswas@Nabendus-MacBook-Air.local>
2020-07-09 18:29:00 +0530

    API Keys removed

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,12 +3,14 @@ import './GoogleAuth.css';
 import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class GoogleAuth extends Component {
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
         window.gapi.client.init({
-            clientId: '779420108644-79ogp656fn7vh26m69funhe1rmrm4t6i.apps.googleusercontent.com',
+            clientId: `${API_KEY}`,
             scope: 'email'
         }).then(() => {
             this.auth = window.gapi.auth2.getAuthInstance();
